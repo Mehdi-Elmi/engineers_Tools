1389ca099e07da39193279f78636e7fedae2813a
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-27 07:32:36 +0330

    Support multiple new workspace windows

--- src/engineers_tools/app/controller.py
+++ src/engineers_tools/app/controller.py
@@ -17,6 +17,7 @@ class AppController(QObject):
         super().__init__()
         self._launcher: LauncherWindow | None = None
         self._module_windows: dict[str, ModuleWindow] = {}
+        self._new_workspace_counter = 0
 
     def show_launcher(self) -> None:
         if self._launcher is None:
@@ -34,14 +35,28 @@ class AppController(QObject):
         window = self._module_windows.get(module.key)
         if window is None:
             window = self._create_module_window(module)
-            window.back_requested.connect(self.show_launcher)
-            self._module_windows[module.key] = window
+            self._register_module_window(module.key, window)
 
         window.show()
         window.raise_()
         window.activateWindow()
         if self._launcher is not None:
             self._launcher.hide()
+
+    def create_new_workspace(self, module: LauncherModule) -> None:
+        self._new_workspace_counter += 1
+        window_key = f"{module.key}:new:{self._new_workspace_counter}"
+        window = self._create_module_window(module)
+        window.setWindowTitle(f"{module.title} - New {self._new_workspace_counter}")
+        self._register_module_window(window_key, window)
+        window.show()
+        window.raise_()
+        window.activateWindow()
+
+    def _register_module_window(self, key: str, window: ModuleWindow) -> None:
+        window.back_requested.connect(self.show_launcher)
+        window.new_workspace_requested.connect(self.create_new_workspace)
+        self._module_windows[key] = window
 
     def _create_module_window(self, module: LauncherModule) -> ModuleWindow:
         create_window = self._load_module_entry_point(module.entry_point)
