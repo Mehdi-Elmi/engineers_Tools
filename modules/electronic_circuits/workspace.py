0fb11669b9415ae12c2d9ef209ab670e1bd9b4f6
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-27 06:02:52 +0330

    Add circuit placeholder workspace

---
+++ modules/electronic_circuits/workspace.py
@@ -0,0 +1,11 @@
+"""Circuit Design workspace placeholder."""
+
+from __future__ import annotations
+
+from src.engineers_tools.app.module_window import ModuleWindow
+from src.engineers_tools.app.modules import LauncherModule
+
+
+class CircuitDesignWorkspace(ModuleWindow):
+    def __init__(self, module: LauncherModule) -> None:
+        super().__init__(module)
