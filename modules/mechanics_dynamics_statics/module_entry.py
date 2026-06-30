460753205c1df4c8cd3136d37406c2ad184613e7
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 14:41:25 +0330

    Apply final interaction policy last

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -6,6 +6,7 @@ from src.engineers_tools.app.module_window import ModuleWindow
 from src.engineers_tools.app.modules import LauncherModule
 
 from .cursor_unification_fixes import apply_cursor_unification_fixes
+from .final_interaction_policy_fixes import apply_final_interaction_policy_fixes
 from .fixed_workspace_dimension_fixes import apply_fixed_workspace_dimension_fixes
 from .interaction_fixes import apply_interaction_fixes
 from .ruler_precision_fixes import apply_ruler_precision_fixes
@@ -21,4 +22,5 @@ def create_window(module: LauncherModule) -> ModuleWindow:
     apply_cursor_unification_fixes()
     apply_fixed_workspace_dimension_fixes()
     apply_ruler_precision_fixes()
+    apply_final_interaction_policy_fixes()
     return EngineeringDesignWorkspace(module)
