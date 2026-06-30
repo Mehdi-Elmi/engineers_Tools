5866a34e75d42aaf58676b412257771540e69fea
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 13:12:21 +0330

    Apply engineering window resize fixes

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -6,9 +6,11 @@ from src.engineers_tools.app.module_window import ModuleWindow
 from src.engineers_tools.app.modules import LauncherModule
 
 from .interaction_fixes import apply_interaction_fixes
+from .window_resize_fixes import apply_window_resize_fixes
 from .workspace import EngineeringDesignWorkspace
 
 
 def create_window(module: LauncherModule) -> ModuleWindow:
+    apply_window_resize_fixes()
     apply_interaction_fixes()
     return EngineeringDesignWorkspace(module)
