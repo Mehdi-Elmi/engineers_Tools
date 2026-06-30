2dfdd702cee012f48e1965ea1a8291f675a90342
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 19:42:46 +0330

    Run page setup properties hotfix last

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -12,6 +12,7 @@ from .final_interaction_policy_fixes import apply_final_interaction_policy_fixes
 from .final_ui_repair_fixes import apply_final_ui_repair_fixes
 from .fixed_workspace_dimension_fixes import apply_fixed_workspace_dimension_fixes
 from .interaction_fixes import apply_interaction_fixes
+from .page_setup_properties_hotfix import apply_page_setup_properties_hotfix
 from .ruler_precision_fixes import apply_ruler_precision_fixes
 from .startbar_cursor_fixes import apply_startbar_cursor_fixes
 from .ui_refinement_fixes import apply_ui_refinement_fixes
@@ -31,4 +32,5 @@ def create_window(module: LauncherModule) -> ModuleWindow:
     apply_current_interaction_cleanup_fixes()
     apply_file_export_project_fixes()
     apply_final_ui_repair_fixes()
+    apply_page_setup_properties_hotfix()
     return EngineeringDesignWorkspace(module)
