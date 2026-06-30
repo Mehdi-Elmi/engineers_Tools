606f739111dae7947da9c46421c083db7ecfab27
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 14:28:46 +0330

    Enable unified engineering cursor style

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from src.engineers_tools.app.module_window import ModuleWindow
 from src.engineers_tools.app.modules import LauncherModule
 
+from .cursor_unification_fixes import apply_cursor_unification_fixes
 from .fixed_workspace_dimension_fixes import apply_fixed_workspace_dimension_fixes
 from .interaction_fixes import apply_interaction_fixes
 from .ruler_precision_fixes import apply_ruler_precision_fixes
@@ -17,6 +18,7 @@ def create_window(module: LauncherModule) -> ModuleWindow:
     apply_window_resize_fixes()
     apply_interaction_fixes()
     apply_ui_refinement_fixes()
+    apply_cursor_unification_fixes()
     apply_fixed_workspace_dimension_fixes()
     apply_ruler_precision_fixes()
     return EngineeringDesignWorkspace(module)
