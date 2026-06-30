338dbc570e468e40bc0e9ab4d855902906cc0c1e
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 20:38:48 +0330

    Run properties grid cleanup patch last

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -13,6 +13,7 @@ from .final_ui_repair_fixes import apply_final_ui_repair_fixes
 from .fixed_workspace_dimension_fixes import apply_fixed_workspace_dimension_fixes
 from .interaction_fixes import apply_interaction_fixes
 from .page_setup_properties_hotfix import apply_page_setup_properties_hotfix
+from .properties_grid_cleanup_patch import apply_properties_grid_cleanup_patch
 from .ruler_precision_fixes import apply_ruler_precision_fixes
 from .startbar_cursor_fixes import apply_startbar_cursor_fixes
 from .ui_refinement_fixes import apply_ui_refinement_fixes
@@ -35,4 +36,5 @@ def create_window(module: LauncherModule) -> ModuleWindow:
     apply_final_ui_repair_fixes()
     apply_page_setup_properties_hotfix()
     apply_unit_grid_properties_final_patch()
+    apply_properties_grid_cleanup_patch()
     return EngineeringDesignWorkspace(module)
