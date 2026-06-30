5df4815b42b927ceb89e9259281077c220639e1b
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 13:50:39 +0330

    Enable fixed workspace dimension scaling

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from src.engineers_tools.app.module_window import ModuleWindow
 from src.engineers_tools.app.modules import LauncherModule
 
+from .fixed_workspace_dimension_fixes import apply_fixed_workspace_dimension_fixes
 from .interaction_fixes import apply_interaction_fixes
 from .ruler_precision_fixes import apply_ruler_precision_fixes
 from .ui_refinement_fixes import apply_ui_refinement_fixes
@@ -16,5 +17,6 @@ def create_window(module: LauncherModule) -> ModuleWindow:
     apply_window_resize_fixes()
     apply_interaction_fixes()
     apply_ui_refinement_fixes()
+    apply_fixed_workspace_dimension_fixes()
     apply_ruler_precision_fixes()
     return EngineeringDesignWorkspace(module)
