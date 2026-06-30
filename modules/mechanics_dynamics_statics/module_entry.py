e532d8a45f8809d01a6e691410990fac9e53ad98
Mehdi-Elmi <elmimehdi362@gmail.com>
2026-06-30 13:48:12 +0330

    Enable engineering UI refinement fixes

--- modules/mechanics_dynamics_statics/module_entry.py
+++ modules/mechanics_dynamics_statics/module_entry.py
@@ -6,6 +6,7 @@ from src.engineers_tools.app.module_window import ModuleWindow
 from src.engineers_tools.app.modules import LauncherModule
 
 from .interaction_fixes import apply_interaction_fixes
+from .ui_refinement_fixes import apply_ui_refinement_fixes
 from .window_resize_fixes import apply_window_resize_fixes
 from .workspace import EngineeringDesignWorkspace
 
@@ -13,4 +14,5 @@ from .workspace import EngineeringDesignWorkspace
 def create_window(module: LauncherModule) -> ModuleWindow:
     apply_window_resize_fixes()
     apply_interaction_fixes()
+    apply_ui_refinement_fixes()
     return EngineeringDesignWorkspace(module)
